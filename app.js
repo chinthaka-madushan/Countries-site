e39d356124553173e11d0777382ca919d7642b92
githubmage2002 <pasinduchithakaa17@gmail.com>
2024-09-16 09:30:55 +0530

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.getElementById("searchResalt").style.display = "none";
 fetch("https://restcountries.com/v3.1/all")
 .then(res=>res.json())
 .then(data=>{
@@ -27,10 +28,63 @@ fetch("https://restcountries.com/v3.1/all")
     tblAllCountries.innerHTML=tblBody;
 })
 function btnSearch(){
+    document.getElementById("searchResalt").style.display = "block";
     let txtSearchValue = document.getElementById("txtSearch").value;
-    console.log(txtSearchValue);
+    fetch(`https://restcountries.com/v3.1/name/${txtSearchValue}`)
+    .then(res=>res.json())
+    .then(data=>{
+         data.forEach(obj=>{
+            document.getElementById("searchResalt").innerHTML = `<tr>
+                                                    <th style="font-size: 30px;">${obj.name.official}</th>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <img src="${obj.flags.png}" height="200px">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>common name : ${obj.name.common}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>capital : ${obj.capital}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>region : ${obj.region}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>subregion : ${obj.subregion}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>latlng : ${obj.latlng}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>borders : ${obj.borders}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                <td><h5>population : ${obj.population}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>area : ${obj.area} km<sup>2</sup></h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>time zone : ${obj.timezones}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>continents : ${obj.continents}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><h5>start of week : ${obj.startOfWeek}</h5></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="${obj.maps.googleMaps}"><h4><i class="bi bi-hand-index-thumb-fill"></i> Google map</h4></a></td>
+                                                </tr>`
+
+        })
+    });
+   
 }
 
-function tblSearch(name){
-    console.log(name);
-}
+
+// function tblSearch(name){
+//     console.log(name);
+// }
